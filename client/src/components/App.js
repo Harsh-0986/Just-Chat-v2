c0d26dff8bcf3a8a98054823bf653939bba63bb2
Harsh Shah <harsh.r.shah2102@gmail.com>
2021-06-12 12:55:44 +0530

    Completed Conversation Context and Sidebar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,13 +4,16 @@ import Login from "./Login";
 import useLocalStorage from "../hooks/useLocalStorage";
 import Dashboard from "./Dashboard";
 import { ContactsProvider } from "../contexts/ContactsProvider";
+import { ConversationsProvider } from "../contexts/ConversationsProvider";
 
 function App() {
   const [id, setId] = useLocalStorage("id");
 
   const dashboard = (
     <ContactsProvider>
-      <Dashboard id={id}></Dashboard>
+      <ConversationsProvider>
+        <Dashboard id={id}></Dashboard>
+      </ConversationsProvider>
     </ContactsProvider>
   );
 
--- client/src/components/NewConversationModal.js
+++ client/src/components/NewConversationModal.js
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+import { useContacts } from "../contexts/ContactsProvider";
+import { useConversations } from "../contexts/ConversationsProvider";
 
-export default function NewConversationModal() {
-  return <div></div>;
+export default function NewConversationModal({ closeModal }) {
+  const [selectedContactIds, setSelectedContactIds] = useState([]);
+  const { contacts } = useContacts();
+  const { createConversation } = useConversations();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    createConversation(selectedContactIds);
+    closeModal();
+  }
+  function handleCheckBoxChange(contactId) {
+    setSelectedContactIds((prevSelectedContactIds) => {
+      if (prevSelectedContactIds.includes(contactId)) {
+        return prevSelectedContactIds.filter((prevId) => {
+          return contactId !== prevId;
+        });
+      } else {
+        return [...prevSelectedContactIds, contactId];
+      }
+    });
+  }
+  return (
+    <>
+      <Modal.Header>Create Conversation</Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          {contacts.map((contact) => (
+            <Form.Group controlId={contact.id} key={contact.id}>
+              <Form.Check
+                type="checkbox"
+                value={selectedContactIds.includes(contact.id)}
+                label={contact.name}
+                onChange={() => handleCheckBoxChange(contact.id)}
+              ></Form.Check>
+            </Form.Group>
+          ))}
+
+          <div class="w-100 align-items-center justify-content-center">
+            <Button
+              type="submit"
+              className="mt-3 align-items-center justify-content-center"
+              style={{ width: "60vw" }}
+            >
+              Create Conversations
+            </Button>
+          </div>
+        </Form>
+      </Modal.Body>
+    </>
+  );
 }
