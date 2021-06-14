2856777608bf482e0b710ab6e09b819bb47763bd
Harsh Shah <harsh.r.shah2102@gmail.com>
2021-06-14 09:08:29 +0530

    Deploying frontend to cloudfare pages and netlify

--- client/src/contexts/SocketProvider.js
+++ client/src/contexts/SocketProvider.js
@@ -11,7 +11,9 @@ export function SocketProvider({ id, children }) {
   const [socket, setSocket] = useState();
 
   useEffect(() => {
-    const newSocket = io("http://localhost:5000", { query: { id } });
+    const newSocket = io("https://just-chat-v2-server.herokuapp.com/", {
+      query: { id },
+    });
     setSocket(newSocket);
 
     return () => newSocket.close();
